c3417bdc42201062e1c108bcfa11be4bae5ec4df
Antorakk <95444282+Antorakk@users.noreply.github.com>
2022-12-17 15:29:11 +0100

    Matchs Views + connection with modal match

---
+++ frontend/src/views/DisplayMatchs.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="container-matchs">
+        <Matchs @emitMatch="displayModalMatch"/>
+        <Match @emitMatch="hideModalMatch" v-if="display_match" :id_match="id_match"/>
+    </div>
+</template>
+
+<script>
+import Matchs from '@/components/Matchs.vue';
+import Match from '@/modals/Match.vue'
+export default {
+    name:"Matchs-view",
+    data:function(){
+        return {
+            display_match : false,
+            id_match : null,
+        }
+    },
+    components:{
+        Matchs,
+        Match
+    },
+    methods:{
+        displayModalMatch(id){
+            this.id_match = id;
+            this.display_match = true;
+        },
+        hideModalMatch(){
+            this.display_match = false;
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
