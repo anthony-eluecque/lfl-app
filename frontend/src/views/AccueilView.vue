1a29091f0e0efe4fffc03e4aaa3459992f023992
Antorakk <95444282+Antorakk@users.noreply.github.com>
2022-12-20 23:14:01 +0100

    Create Accueil component

--- frontend/src/views/AccueilView.vue
+++ frontend/src/views/AccueilView.vue
@@ -1,5 +1,16 @@
 <template>
-    <NavBar/>
+    <div class="accueil-container">
+        <NavBar/>
+        <div class="content">
+            <p>Retrouvez toutes les statistiques  de la LFL Spring 2022 !</p>
+        </div>
+        <div id="footer">
+            <img src="@/assets/fond-accueil.png">
+        </div>
+        <div class="madeBy">
+            <p class="auteur">Auteurs : Frédéric DOURNEL - Anthony ELUECQUE - Benjamin FOURNIER</p> 
+        </div>
+    </div>
 </template>
 
 
@@ -9,9 +20,40 @@ import NavBar from '@/components/navBar.vue';
 export default {
     name:"Vue-Accueil",
     components:{NavBar}
-}
+}   
 </script>
 
 <style lang="scss" scoped>
+.accueil-container{
+    display: flex;
 
+    #footer {
+        position:fixed;
+        right:0;
+        bottom:0;
+        margin:0;
+        padding:0;
+        width: fit-content;
+        height: 715px;
+    }
+
+    .madeBy{
+        position: fixed;
+        bottom: 0;
+        height: 20px;
+        margin-left: 250px;
+    }
+
+    .content p{
+        font-family: 'Bebas Neue', cursive;
+        font-size:500%;
+        color:rgb(50, 73, 96);
+    }
+
+    .content{
+        width: 70vw;
+        text-align: left;
+        margin-left: 50px;
+    }
+}
 </style>
